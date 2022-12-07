96dfa3c05a3652c5f32c67c8fb48bd393f045f44
wboler <wboler@iupui.edu>
2022-12-06 22:00:35 -0500

    Day 4 Part 2

--- day4/part1.py
+++ day4/part1.py
@@ -3,6 +3,7 @@
 import argparse, os, sys
 import hashlib
 
+
 def load_data(input_filename:str):
 
     assert(os.path.isfile(input_filename))
@@ -16,28 +17,30 @@ def load_data(input_filename:str):
             data.append(r)
     return data
 
+
 def get_md5(msg:str):
     m = hashlib.md5()
     m.update(msg.encode('UTF-8'))
     return m.hexdigest()
 
-def get_salted_hash(row: str):
+
+def get_salted_hash(row: str, n=5):
     idx = 0
     salt_hash = row
+    zero_check = ''.join('0' for _ in range(n))
 
-    while (get_md5(salt_hash)[:5] != '00000'):
+    while (get_md5(salt_hash)[:n] != zero_check):
         idx += 1
         salt_hash = f"{row}{idx}"
     return salt_hash
 
 
-
-def get_salted_hashes(data:list):
+def get_salted_hashes(data:list, n=5):
 
     salt_hashes = list()
 
     for row in data:
-        salt_hashes.append(get_salted_hash(row))
+        salt_hashes.append(get_salted_hash(row, n))
 
     return salt_hashes
 
@@ -46,10 +49,11 @@ def main():
     
     parser = argparse.ArgumentParser(description="Using python because of hashlib")
     parser.add_argument('input_filename', type=str, help="list of inputs - file")
+    parser.add_argument('-n', type=int, default=5)
     args = parser.parse_args()
 
     data = load_data(args.input_filename)
-    salt_hashes = get_salted_hashes(data)
+    salt_hashes = get_salted_hashes(data, args.n)
 
     for s in salt_hashes:
         print(s)
